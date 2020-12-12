a9ddbdbb90f133444cec2415e59c0cf9a6a93548
harvey magana <heyharv2012@gmail.com>
2020-12-12 07:40:34 -0800

    Added the useEffect in the Bubble Page and set up a get request to consume the API data. This is a good working version.

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react";
+import axiosWithAuth from '../utils/axioxWithAuth';
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 import {
   Container,
-  Header,
-  Menu,
-  Segment,
-  Form, 
-  Button
+  Menu
 } from 'semantic-ui-react';
 
 import Bubbles from "./Bubbles";
@@ -18,9 +15,22 @@ const BubblePage = (props) => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+    axiosWithAuth().get("http://localhost:5000/api/colors")
+    .then(res => {
+        console.log(res)
+        const colors = res.data;
+        setColorList = ({ colors })
+        props.history.push('/bubble-page')
+    })
+    .catch(err => {
+        console.log(err.response);
+    })
+  }, []);
+
   const logout = () => {
     localStorage.removeItem("token");
-    props.history.push('/login')
+    props.history.push('/login');
   }
 
   return (
